fc6ef95dab095be45d8679d7497be91d258cbdcd
nikrossin <bns17u144@student.bmstu.ru>
2022-02-09 01:22:10 +0300

    Add task19

---
+++ 19/task19_v1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func stringReverse(str string) string {
+	symbols := []rune(str)
+
+	for i := 0; i < int(len(symbols)/2); i++ {
+		symbols[i], symbols[len(symbols)-1-i] = symbols[len(symbols)-1-i], symbols[i]
+	}
+	return string(symbols)
+}
+
+func main() {
+	var s = "HeLlo world, 世 界! Привет мир"
+	fmt.Println(stringReverse(s))
+}
