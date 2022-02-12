f6af6e40aa4da844ee85b1a1ad0c57f8a517000d
nikrossin <bns17u144@student.bmstu.ru>
2022-02-12 03:55:50 +0300

    Add answers

--- test.go
+++ test.go
@@ -2,19 +2,13 @@ package main
 
 import "fmt"
 
-func update(p *int) {
-	fmt.Println(*p)
-	b := 2
-	p = &b
-	fmt.Println(*p)
+type Test struct {
+	id *int
 }
 
 func main() {
-	var (
-		a = 1
-		p = &a
-	)
-	fmt.Println(*p)
-	update(p)
-	fmt.Println(*p)
+	p := new([]int)
+	a := make([]int, 0)
+	var b []int
+	fmt.Println(cap(*p), len(*p), cap(a), len(a), cap(b), len(b))
 }
